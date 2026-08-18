83801a16ec3d298e1c1d2861e5432df29d82de7f
YEMO007 <3341372341@qq.com>
2026-08-18 21:22:40 +0800

    Add configuration file for settings

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  LABEL_NAME: '夜魔文化',            // 强制厂牌名，不允许改
+  QQ_OPEN_BASE: 'https://c.y.qq.com/v8/fcg-bin',
+  // QQ音乐开放平台你申请后拿到的（如用自有代理）
+  QQ_COOKIE: process.env.QQ_COOKIE || '', 
+};
